21a8676ce484d7b6a601d20840c87aa942ec81b8
RandomJavaSkid <68204525+Nickyesulol@users.noreply.github.com>
2023-01-28 20:58:27 +0100

    added File and license

---
+++ TimeUtil.java
@@ -0,0 +1,17 @@
+package de.olympians.lookatmyeyes.util;
+
+public class TimeUtil {
+
+    private long time = -1L;
+
+    public boolean hasTimePassed(final long ms) { return System.currentTimeMillis() >= time + ms; }
+
+    public long hasTimeLeft(final long ms) { return (ms + time) - System.currentTimeMillis(); }
+
+    public void reset() { time = System.currentTimeMillis(); }
+
+    public long getCurrentMS() { return System.currentTimeMillis(); }
+
+    public final long getElapsedTime() { return this.getCurrentMS() - this.time; }
+
+}
